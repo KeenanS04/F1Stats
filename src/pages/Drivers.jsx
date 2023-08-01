c205c635544aacd5fac47ab21c4b03388d1fd3e0
KeenanS04 <keenandablob@gmail.com>
2023-08-01 15:10:14 -0500

    API calls tests in drivers page

--- src/pages/Drivers.jsx
+++ src/pages/Drivers.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
+const API_URL = "https://ergast.com/api/f1/current/driverStandings.json";
+
 function Drivers() {
+    const [drivers, setDrivers] = useState([]);
+    const [season, setSeason] = useState("");
+    useEffect(() => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => {
+                setDrivers(data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+                setSeason(data.MRData.StandingsTable.season);
+                // console.log(drivers);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+    let driversNames = drivers.map(data => (
+            <p key={data.Driver.driverId}>
+                {data.positionText}. {data.Driver.givenName} {data.Driver.familyName}
+            </p>
+        )
+    )
     return (
         <>
-            <h1>Drivers</h1>
+            <h1>Drivers Standings for the {season} season</h1>
+            {driversNames}
         </>
     );
 }
